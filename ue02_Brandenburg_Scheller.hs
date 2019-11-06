33feac155889f8d36d2f4c35896ed714a892a15d
FrodoProductions <freddy@aerofloyd.de>
2019-11-06 15:29:22 +0100

    Aufgabe 2

--- ue02_Brandenburg_Scheller.hs
+++ ue02_Brandenburg_Scheller.hs
@@ -1,9 +1,24 @@
 {- Frederick Brandenburg, Ferdinand Markward Scheller
 Tutoriumsnummer: 03 -}
 
+-- AUFGABE 2
+
+sumOfTeiler :: Int -> Int -> Int -> Int
+-- Wenn das Programm bei 0 ankommt, wird es terminiert,
+-- da kein weiterer Funktionsaufruf ausgeführt wird
+sumOfTeiler 0 a b = 0
+-- Wenn n durch a oder b teilbar ist, wird sumOfTeiler
+-- rekursiv mit n-1 aufgerufen und zu n addiert,
+-- ansonsten wird nur sumOfTeiler mit n-1 aufgerufen,
+-- ohne dass das Ergebnis addiert wird.
+sumOfTeiler n a b | n `mod` a == 0 || n `mod` b == 0 = n + sumOfTeiler (n-1) a b
+                  | otherwise = sumOfTeiler (n-1) a b
+
 -- AUFGABE 3
 
--- Definition einer Hilfsfunktion factorial für n!
+-- Definition einer Hilfsfunktion factorial,
+-- damit n! einfach berechnet werden kann.
+
 factorial 0 = 1
 factorial n = n * factorial (n-1)
 
