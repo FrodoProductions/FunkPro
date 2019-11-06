b0c4f2a3a30ec6d287fa9e8052be6ba8cc630fdd
FrodoProductions <freddy@aerofloyd.de>
2019-11-06 15:10:25 +0100

    Aufgabe 3

--- ue02_Brandenburg_Scheller.hs
+++ ue02_Brandenburg_Scheller.hs
@@ -1,2 +1,12 @@
 {- Frederick Brandenburg, Ferdinand Markward Scheller
 Tutoriumsnummer: 03 -}
+
+-- AUFGABE 3
+
+-- Definition einer Hilfsfunktion factorial für n!
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+euler :: Double -> Double
+euler 0 = 1
+euler n = 1 / (factorial n) + euler (n-1)
