27165c86a99b173107025a268c8770000e017de4
FrodoProductions <freddy@aerofloyd.de>
2019-11-15 11:31:55 +0100

    Aufgabe 4

--- ue03_Brandenburg_Scheller.hs
+++ ue03_Brandenburg_Scheller.hs
@@ -1,2 +1,18 @@
 {- Frederick Brandenburg, Ferdinand Markward Scheller
 Tutoriumsnummer: 03 -}
+
+-- AUFGABE 4
+
+fromDecTo :: Int -> Int -> [Int]
+fromDecTo b num = reverse (dec2 b num)
+
+dec2 :: Int -> Int -> [Int]
+dec2 b num | b<1 || b>9 = error "Base must be between 0 and 10!"
+           | num<1 = []
+           | otherwise = (mod num b) : dec2 b (div num b)
+
+{- Testlauf
+
+fromDecTo 5 555
+> [4,2,1,0]
+-}
