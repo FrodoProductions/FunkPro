1f81a840a4f5978bdd755b1d4b7f839ac2e2b443
FrodoProductions <freddy@aerofloyd.de>
2019-11-29 13:06:10 +0100

    Aufgabe 1

--- ue05_Brandenburg_Scheller.hs
+++ ue05_Brandenburg_Scheller.hs
@@ -2,3 +2,26 @@
 Tutoriumsnummer: 03 -}
 
 -- AUFGABE 1
+
+-- Laufzeitkomplexität: O(n)+O(n)∈O(n)
+-- Zunächst wird ein e als das Ergebnis von calculateFirst definiert.
+-- Dieses wird dann als neues Element an die zurückzugebende Liste angefügt und die Funktion
+-- ruft sich selber rekursiv auf, wobei die betrachtete Liste durch deleteElem modifiziert wird.
+selectSort :: Ord a => (a -> a -> Bool) -> [a] -> [a]
+selectSort _ [] = []
+selectSort f xs = let
+  e = calculateFirst f xs
+  in e : (selectSort f (deleteElem e xs))
+  where
+   -- Laufzeitkomplexität: O(n)
+   -- Diese Funktion wendet die eingegebene Funktion auf die jeweils ersten beiden Elmenete
+   -- der Liste an und entfernt das "unterlegene" Element. Dieser Prozess wird fortgesetzt,
+   -- bis nur noch eines übrig ist, welches dann auch zurückgegeben wird.
+   calculateFirst :: Ord a => (a -> a -> Bool) -> [a] -> a
+   calculateFirst f (x:[]) = x
+   calculateFirst f (x:y:xs) | f x y == True = calculateFirst f (x:xs)
+                             | otherwise = calculateFirst f (y:xs)
+   -- Laufzeitkomplexität: O(n)
+   -- Diese Funktion verwendet die filter Funktion, um eine Liste aller Elemente =/ e zu erstellen.
+   deleteElem :: Eq a => a -> [a] -> [a]
+   deleteElem e xs = filter (/=e) xs
